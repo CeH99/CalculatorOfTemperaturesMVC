10d21689f42caf79740313da3e83c36d553d1213
Semen <senya183381@gmail.com>
2025-06-25 13:02:52 +0300

    update

--- 6/build/Desktop_Qt_6_9_1-Debug/.qt/appCalculator_qml.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appCalculator")
-set(working_dir "/home/user/Qt_Projects/6")
-set(src_and_dest_list
-    "/home/user/Qt_Projects/6/Main.qml"
-    "/home/user/Qt_Projects/6/build/Desktop_Qt_6_9_1-Debug/Calculator/Main.qml"
-
-)
-set(timestamp_file "/home/user/Qt_Projects/6/build/Desktop_Qt_6_9_1-Debug/.qt/appCalculator_qml.txt")

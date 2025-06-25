b9c61c89961425580d41fd99c161f804e69e25df
Semen <senya183381@gmail.com>
2025-06-25 14:55:25 +0300

    add screenshots

---
+++ 6/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/appCalculator_qml.cmake
@@ -0,0 +1,9 @@
+
+set(target "appCalculator")
+set(working_dir "S:/HomeWorks-Hillel/6")
+set(src_and_dest_list
+    "S:/HomeWorks-Hillel/6/Main.qml"
+    "S:/HomeWorks-Hillel/6/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/Calculator/Main.qml"
+
+)
+set(timestamp_file "S:/HomeWorks-Hillel/6/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/appCalculator_qml.txt")
---
+++ 6/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/appCalculator_res.cmake
@@ -0,0 +1,7 @@
+
+set(target "appCalculator")
+set(working_dir "S:/HomeWorks-Hillel/6")
+set(src_and_dest_list
+
+)
+set(timestamp_file "S:/HomeWorks-Hillel/6/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/appCalculator_res.txt")
